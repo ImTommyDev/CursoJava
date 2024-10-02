ea5f44d1f53aaac6b52fa1e6083627929a231b01
ImTommyDev <tommy.devlpr@gmail.com>
2024-10-02 22:28:50 +0200

    #1 Primera lección: Hola mundo y reto para poner a prueba al usuario.

---
+++ PrimerosPasos/src/Clases/Primera_HolaMundo.java
@@ -0,0 +1,10 @@
+package Clases;
+
+public class Primera_HolaMundo {
+    public static void main(String[] args) { //escribe "psvm" y pulsa TAB para crear el método principal
+        System.out.println("Hola mundo!"); //escribe "sout" y pulsa TAB para escribir el método que permite mostrar mensajes por consola
+    }
+}
+//Pulsa Mayus + F10 para ejecutar el código (o dale al play arriba a la derecha)
+
+//EJERCICIO -> En la carpeta "Ejercicios" encontrarás el primer reto, suerte!!
---
+++ PrimerosPasos/src/Ejercicios/Primero_TuNombre.java
@@ -0,0 +1,6 @@
+package Ejercicios;
+
+//Haz un programa que muestre tu nombre y tus apellidos por pantalla
+//IMPORTANTE -> Es posible que se ejecute el código anterior, si esto es así, pulsa a la izquierda del botón del play y selecciona "current file"
+public class Primero_TuNombre {
+}
